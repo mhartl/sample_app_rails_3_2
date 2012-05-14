9bb5af4e6dcae2f8635d45c332850d9a0094280b
Michael Hartl <michael@michaelhartl.com>
2012-05-14 14:55:02 -0700

    Add Cucumber features

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,11 +18,11 @@ describe "AuthenticationPages" do
       before { click_button "Sign in" }
 
       it { should have_selector('title', text: 'Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should have_error_message }
 
       describe "after visiting another page" do
         before { click_link "Home" }
-        it { should_not have_selector('div.alert.alert-error') }
+        it { should_not have_error_message }
       end
     end
 
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,7 @@
 include ApplicationHelper
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: 'Invalid')
+  end
+end
