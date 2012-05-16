8ccc16bf3cb501b53231562ec09f233a8ca5c515
Michael Hartl <michael@michaelhartl.com>
2012-05-16 13:18:20 -0700

    Minor polish

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,7 +29,7 @@ describe "AuthenticationPages" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",     with: user.email
+        fill_in "Email",    with: user.email
         fill_in "Password", with: user.password
         click_button "Sign in"
       end
