ee7cf0e60694e0548e48a28e4ba67d1a57c3f612
Michael Hartl <michael@michaelhartl.com>
2012-05-19 12:23:28 -0700

    Eliminate an unnecessary assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       sign_in @user
       flash[:success] = "Profile updated"
