5eb70ff37de271d952ad58320ab8cc506bc58246
Michael Hartl <michael@michaelhartl.com>
2012-05-21 00:54:49 -0700

    Minor fixes

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -11,6 +11,6 @@ class Micropost < ActiveRecord::Base
     followed_user_ids = "SELECT followed_id FROM relationships
                          WHERE follower_id = :user_id"
     where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
-          { user_id: user.id })
+          user_id: user.id)
   end
 end
