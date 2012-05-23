990d0c6e904fd778f54ece34ec42e2733d410d80
Michael Hartl <michael@michaelhartl.com>
2012-05-23 13:30:23 -0700

    Typo fixes

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -26,12 +26,12 @@ describe Relationship do
     its(:followed) { should == followed }
   end
 
-  describe "when follower it is not present" do
+  describe "when follower id is not present" do
     before { relationship.follower_id = nil }
     it { should_not be_valid }
   end
 
-  describe "when followed it is not present" do
+  describe "when followed id is not present" do
     before { relationship.followed_id = nil }
     it { should_not be_valid }
   end
